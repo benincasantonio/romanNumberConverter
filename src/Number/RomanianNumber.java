e0849e6a58366c97818b8b3855815f4141196b1a
Antonio Benincasa <benincasaantonio98@gmail.com>
2017-09-09 20:14:51 +0200

    Basic Package and Class created

---
+++ src/Number/RomanianNumber.java
@@ -0,0 +1,16 @@
+package Number;
+
+public class RomanianNumber {
+
+    public RomanianNumber(){
+
+    }
+
+    public String toRomanianNumber(int num){
+        return "";
+    }
+
+    public int toIntegerNumber(String romaninaNum){
+        return 1;
+    }
+}
